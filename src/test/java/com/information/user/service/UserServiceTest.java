5c2be32318fa2a64c3032ad9ab766b40f149066c
Natsumi <nat.k@kino41.local>
2024-05-19 17:38:33 +0900

    post,delete処理の単体テスト実装

--- src/test/java/com/information/user/service/UserServiceTest.java
+++ src/test/java/com/information/user/service/UserServiceTest.java
@@ -45,11 +45,29 @@ class UserServiceTest {
         doReturn(Optional.of(new User(1, "suzuki", "1973/07/22"))).when(userMapper).findById(1);
         User actual = userService.findById(1);
         assertThat(actual).isEqualTo(new User(1, "suzuki", "1973/07/22"));
+        verify(userMapper).findById(1);
     }
 
     @Test
     void 存在しないユーザーのIDを指定したときに例外を返すこと() {
         doReturn(Optional.empty()).when(userMapper).findById(0);
         assertThrows(UserNotFoundException.class, () -> userService.findById(0));
+    }
+
+    @Test
+    void 新規のユーザーをIDと紐付けて正常に登録できること() {
+        User newUser = new User(4, "hayashi", "1992/03/09");
+        userService.insert(newUser);
+        verify(userMapper).insert(newUser);
+    }
+
+    @Test
+    void 存在するユーザーのIDを指定したときに正常にユーザーを削除できること() {
+        doReturn(Optional.of(new User(1, "suzuki", "1973/07/22"))).when(userMapper).findById(1);
+        User actual = userService.findById(1);
+        assertThat(actual).isEqualTo(new User(1, "suzuki", "1973/07/22"));
+        doReturn(1).when(userMapper).deleteById(1);
+        userService.delete(1);
+        verify(userMapper).deleteById(1);
     }
 }
